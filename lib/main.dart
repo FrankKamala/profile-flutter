db940b0cf301cddfea1c03e612a5b7b6dbb2659b
FrankKamala <frankkamala@gmail.com>
2021-04-28 12:47:51 +0300

    add child

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black12,
-          title: Text('Home'),
+          backgroundColor: Colors.green,
+          title: Text('Home',textDirection: TextDirection.ltr,style: TextStyle(color: Colors.white),),
+
+        ),
+        body:SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  color: Colors.green,
+                  height: 200,
+                  width: 200,
+// child:AssetImage(assetName),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
